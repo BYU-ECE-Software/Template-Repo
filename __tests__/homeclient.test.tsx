16a21a8556aa1d19d59e3ecd6cff83566495ecae
Roman Vish <romanv7@byu.edu>
2026-04-09 14:36:58 -0600

    Trying this for updated HomeClient instead of default page

--- __tests__/homeclient.test.tsx
+++ __tests__/homeclient.test.tsx
@@ -1,9 +1,9 @@
 import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Page from '../app/page';
+import HomeClient from '@/components/HomeClient';
 
 test('Page', () => {
-  render(<Page />);
+  render(<HomeClient />);
   expect(
     screen.getByRole('heading', { level: 1, name: 'To get started, edit the page.tsx file.' }),
   ).toBeDefined();
